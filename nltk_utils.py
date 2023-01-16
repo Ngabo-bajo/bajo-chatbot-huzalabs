2ee06709dc91e0580c79b0a3c888027a7a48fd61
Cali431 <ngabonzizabajo1@gmail.com>
2023-01-16 10:17:05 +0200

    updating the nltk_utils and adding dockerfile

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,17 +1,14 @@
 import numpy as np
 import nltk
-# nltk.download('punkt')
+nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
-
 def tokenize(sentence):
     """
     split sentence into array of words/tokens
     a token can be a word or punctuation character, or number
     """
     return nltk.word_tokenize(sentence)
-
-
 def stem(word):
     """
     stemming = find the root form of the word
@@ -21,8 +18,6 @@ def stem(word):
     -> ["organ", "organ", "organ"]
     """
     return stemmer.stem(word.lower())
-
-
 def bag_of_words(tokenized_sentence, words):
     """
     return bag of words array:
@@ -37,7 +32,6 @@ def bag_of_words(tokenized_sentence, words):
     # initialize bag with 0 for each word
     bag = np.zeros(len(words), dtype=np.float32)
     for idx, w in enumerate(words):
-        if w in sentence_words: 
+        if w in sentence_words:
             bag[idx] = 1
-
     return bag
